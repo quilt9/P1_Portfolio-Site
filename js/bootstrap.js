f59cc7597f23c64c31677876b9c5ca3e3f4ff79a
quilt9 <helen@quilt9.com>
2015-09-30 23:21:34 -0400

    Modify the hero area
    Instead of image now it is just font.

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -5,8 +5,8 @@
  */
 
 /*!
- * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=6391bb10e7975952e32e)
- * Config saved to config.json and https://gist.github.com/6391bb10e7975952e32e
+ * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=00fff0e8a1a4802d8c11)
+ * Config saved to config.json and https://gist.github.com/00fff0e8a1a4802d8c11
  */
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
